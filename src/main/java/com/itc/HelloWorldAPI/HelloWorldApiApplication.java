d739741ee4a407045b8a10b0675f0b19d4e9cc60
saravanan.1.p <saravanan.1.p@niit.com>
2024-09-22 05:59:08 +0000

    soln given

---
+++ src/main/java/com/itc/HelloWorldAPI/HelloWorldApiApplication.java
@@ -0,0 +1,23 @@
+package com.itc.HelloWorldAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("api/v1")
+public class HelloWorldApiApplication {
+
+	@GetMapping("getData")
+	public String getHello(){
+		return "welcome to itc";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApiApplication.class, args);
+	}
+
+}
